1b131339e977ac811e83eecabc12aa1b7cae4a26
Christopher Stevers <72156679+Christopher-Stevers@users.noreply.github.com>
2021-11-14 23:44:48 -0500

    added db connection script

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,16 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
+const db = new Sequelize(process.env.DATABASE_URL || "postgres://postgres:compost@localhost:5432/messenger", {
   logging: false
 });
-
+const asyncWrapper = async()=>{
+  
+try {
+  await db.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
+}
+asyncWrapper();
 module.exports = db;
