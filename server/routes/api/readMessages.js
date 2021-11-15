1a69add8aa16e1c026b1768852e2d8d4ab3116ba
Christopher Stevers <72156679+Christopher-Stevers@users.noreply.github.com>
2021-11-15 17:53:31 -0500

    added route for read messages

--- server/routes/api/readMessages.js
+++ server/routes/api/readMessages.js
@@ -2,8 +2,22 @@ const router = require("express").Router();
 const { Conversation, Message } = require("../../db/models");
 const onlineUsers = require("../../onlineUsers");
 
-// expects {recipientId, text, conversationId } in body (conversationId will be null if no conversation exists yet)
+// expects { conversationId } in body (conversationId will be null if no conversation exists yet)
 router.post("/", async (req, res, next) => {
+
+    try{
+
+        if (!req.user) {
+            return res.sendStatus(401);
+          }
+          if(!conversation){
+              next()
+
+          }
+    }
+    catch(err){
+        next(err);
+    }
 });
 
 module.exports = router;
