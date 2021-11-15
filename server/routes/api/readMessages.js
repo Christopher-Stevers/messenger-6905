bc931ba438f781fc6308e817701af808bf071f67
Christopher Stevers <72156679+Christopher-Stevers@users.noreply.github.com>
2021-11-15 12:11:16 -0500

    added api route

---
+++ server/routes/api/readMessages.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+const { Conversation, Message } = require("../../db/models");
+const onlineUsers = require("../../onlineUsers");
+
+// expects {recipientId, text, conversationId } in body (conversationId will be null if no conversation exists yet)
+router.post("/", async (req, res, next) => {
+});
+
+module.exports = router;
