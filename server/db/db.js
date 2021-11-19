61a1f20269ca7db81d649ae357d784a36820e417
Christopher Stevers <72156679+Christopher-Stevers@users.noreply.github.com>
2021-11-19 11:45:02 -0500

    resolving merge issue from master

--- server/db/db.js
+++ server/db/db.js
@@ -1,11 +1,6 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
-<<<<<<< HEAD
-
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-=======
 const db = new Sequelize(process.env.DATABASE_URL , {
->>>>>>> origin/main
   logging: false
 });
 module.exports = db;
