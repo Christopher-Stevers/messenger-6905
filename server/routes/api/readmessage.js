5bf14c40a5179dbff064500cecb9ed7840ba0ccd
Christopher Stevers <72156679+Christopher-Stevers@users.noreply.github.com>
2021-11-19 11:34:14 -0500

    add endpoint for reading messages

--- server/routes/api/readmessage.js
+++ server/routes/api/readmessage.js
@@ -2,16 +2,22 @@ const router = require("express").Router();
 const { Conversation, Message } = require("../../db/models");
 const onlineUsers = require("../../onlineUsers");
 
-// expects { conversationId } in body (conversationId will be null if no conversation exists yet)
-router.get("/", async (req, res, next) => {
-    console.log("exec");
-
-    try{
-        console.log(req);
-        console.log("hi hop");
-        res.json({hi: "hop"});
+// expects { senderId, conversationId,  } in body (conversationId will be null if no conversation exists yet)
+router.post("/", async (req, res, next) => {
+    const { senderId, conversationId } = req.body;
+    try {
+        if (conversationId&&senderId) {
+            const result = await Message.update({ read: true }, {
+                where: {
+                    senderId: senderId,
+                    conversationId,
+                    read: false,
+                }
+            })
+        }
+        res.json({ conversationId });
     }
-    catch(err){
+    catch (err) {
         next(err);
     }
 });
