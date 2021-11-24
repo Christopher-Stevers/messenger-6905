2859b8a64193bae4a325eb8850b0ff1ae0e04e8b
Christopher Stevers <72156679+Christopher-Stevers@users.noreply.github.com>
2021-11-24 10:12:37 -0500

    tweaked models

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -6,16 +6,16 @@ const Unread = require("./unread");
 
 // associations
 
-User.hasMany(Conversation, { through: Group });
-Conversation.hasMany(User, { through: Group });
+User.belongsToMany(Conversation, { through: Group });
+Conversation.belongsToMany(User, { through: Group });
 Message.belongsTo(Conversation);
 Conversation.hasMany(Message);
 User.hasMany(Message, { as: "Sender", foreignKey: "senderId" })
 Message.belongsTo(User)
 // Many Users to many Unreads.
 // Each Unread will be deleted when the User reads the associated message.
-User.hasMany(Message, { through: Unread });
-Message.hasMany(User, { through: Unread });
+User.belongsToMany(Message, { through: Unread });
+Message.belongsToMany(User, { through: Unread });
 
 module.exports = {
   User,
